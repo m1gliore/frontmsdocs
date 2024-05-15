67ff3fecc735bae3b6a04b5a50c0b27b0e505a01
m1gliore <raintruwalker@gmail.com>
2024-05-15 22:39:43 +0300

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,61 @@
+import {TooltipProps} from "recharts";
+
+export type Document = {
+    idDocument: number
+    fileName: string
+    file_in_byte: string
+    employeeDTO: Employee
+    edocumentType: string
+}
+
+export type Employee = {
+    idEmployee: number
+    surname: string
+    name: string
+    patronymic: string
+}
+
+export type Task = {
+    idTask: number
+    nameTask: string
+    employees: Array<EmployeeList>
+    sequentialTaskNodeDTOS: Array<TaskNode>
+}
+
+export type TaskNode = {
+    idSequentialTaskNode: number
+    name_node: string
+}
+
+export type EmployeeList = {
+    name: string
+    surname: string
+    patronymic: string
+}
+
+export interface DecodedToken {
+    isAdmin: boolean
+}
+
+export interface CustomTooltipProps extends TooltipProps<any, any> {
+    active?: boolean
+    payload?: Array<{ name: string; value: number }>
+}
+
+export type Notification = {
+    idNotification: number
+    taskMessage: string
+}
+
+export interface TaskName {
+    employeeId: string
+    taskName: string
+}
+
+export type User = {
+    idUser: number
+    active: boolean
+    employeeDTO: Employee
+    password: string
+    username: string
+}
